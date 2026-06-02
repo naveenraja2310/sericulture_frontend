d3ad186aaeeb17adccc48a64c2552e0d46d5d8f3
naveenraja2310 <109686235+naveenraja2310@users.noreply.github.com>
2026-06-02 17:07:04 +0530

    firebase fix

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
-import { initNotifications } from "./firebase";
-
-initNotifications();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
