e67c6a012a99bf7dba08c220716f603d9d193924
naveenraja2310 <109686235+naveenraja2310@users.noreply.github.com>
2026-06-06 11:45:15 +0530

    autoupdate

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: "prompt",
+      registerType: "autoUpdate",
       manifest: {
         name: "Sericulture IoT",
         short_name: "Sericulture",
